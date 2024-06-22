886e272a20298cb23f56d098133f5e51703fa637
Grant-Ward-code <55935404+Grant-Ward-code@users.noreply.github.com>
2024-06-22 12:55:46 +0800

    Initial Commit

---
+++ focus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  focus
+//
+//  Created by bytedance on 2024/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ focus/focusApp.swift
@@ -0,0 +1,17 @@
+//
+//  focusApp.swift
+//  focus
+//
+//  Created by bytedance on 2024/6/22.
+//
+
+import SwiftUI
+
+@main
+struct focusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
